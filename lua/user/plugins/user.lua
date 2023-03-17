fa19666d328d514d9a006efe9c64a9aa41d3bf72
MatyTan <matytan@bituniverse.org>
2023-03-17 16:52:02 +0800

    add theme

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -11,6 +11,15 @@ return {
   -- },
 
   {
+    "folke/tokyonight.nvim",
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  {
     "APZelos/blamer.nvim",
     lazy = false,
     config = function()
