c2b2ba7f19c84381d35fe16c5b4414ffd50ce5bb
MatyTan <1396568121@qq.com>
2023-03-17 22:05:24 +0800

    fix no pair

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -56,15 +56,15 @@ return {
     },
   },
   -- Configure require("lazy").setup() options
-  lazy = {
-    defaults = { lazy = true },
-    performance = {
-      rtp = {
-        -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin", "matchparen" },
-      },
-    },
-  },
+  -- lazy = {
+  --   defaults = { lazy = true },
+  --   performance = {
+  --     rtp = {
+  --       -- customize default disabled vim plugins
+  --       disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin", "matchparen" },
+  --     },
+  --   },
+  -- },
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
