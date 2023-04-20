f7f0ff1051a47ceb5da77a34de074507d50c60a8
MatyTan <matytan@bituniverse.org>
2023-04-20 14:09:53 +0800

    add codeium

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -45,6 +45,17 @@ return {
     "github/copilot.vim",
   },
   {
+    "Exafunction/codeium.vim",
+    event = "InsertEnter",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end
+  },
+  {
     "gcmt/wildfire.vim",
     lazy = false,
   },
